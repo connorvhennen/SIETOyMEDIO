5f7831dcb074736fdead2366f27a13af54a7f0bd
connorvhennen <connorvhennen@ucla.edu>
2017-01-23 20:23:56 -0800

    Initial Commit

---
+++ sietoYmedio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sietoYmedio
+//
+//  Created by Connor Hennen on 1/23/17.
+//  Copyright © 2017 Connor Hennen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
